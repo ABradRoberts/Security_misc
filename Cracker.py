8fc58a8aef2313b8c583cb750dcddfb10860a9cc
bradr555 <bradr555@iastate.edu>
2019-10-11 01:05:36 -0500

    Cleaned and added todo's that'll never actually get done...

--- Cracker.py
+++ Cracker.py
@@ -1,28 +1,38 @@
-#!/bin/python3
+#!/usr/bin/python3
+# Author: Brad Roberts
 
-# Author: Allen Bradley Roberts
+# TODO:
+# 1. This can easily be multi-threaded in several ways
+# 2. Prolly should error check
+# 3. Ya know, handle args
 
 import os
 import time
 
+hash_file = "hash.txt"
+wordlist = "wordlist2.txt"
+
 start = time.time()
+count = 0
+with open(hash_file, "r") as shadow:
+    for line in shadow:
+        count += 1
+        name = line.split(':')[0]
+        hash = line.split(':')[1]
+        hash_algo = hash.split('$')[1]
+        salt = hash.split('$')[2]
+        print("Starting password for", name)
 
-with open("hash.txt", "r") as shadow:
-    hash=shadow.readline().split(':')[1]
+        with open(wordlist, "r") as wl:
+            print("Searching...", flush=True)
+            for word in wl:
+                response = os.popen("/usr/bin/openssl passwd -{} -salt {} {}".format(hash_algo, salt, word)).read()
 
-hash_algo = hash.split('$')[1]
-salt = hash.split('$')[2]
-
-with open("wordlist.txt", "r") as wordlist:
-    print("Searching...", flush=True)
-    for word in wordlist:
-        response = os.popen("/usr/bin/openssl passwd -{} -salt {} {}".format(hash_algo, salt, word)).read()
-
-        if response.strip() == hash:
-            print("[+] Password found: {}".format(word))
-            end = time.time()
-            print("Execution time: %s", str(end-start))
-            exit(0)
+            if response.strip() == hash:
+                print("[+] Password found: {}".format(word))
+                end = time.time()
+                print("Execution time: %s", str(end-start))
+                exit(0)
 
 print("[-] Password was not in the provided wordlist.")
 
